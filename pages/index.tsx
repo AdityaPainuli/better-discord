f21041cb141a874a27b7e2f24a90f23ef5db54f1
Aditya Painuli <adityapainuli2004@gmail.com>
2022-08-15 18:50:14 +0530

    feature� : (working on signin page)�

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,67 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Discord Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
+      <div>
+        <Image
+          src="/../public/background.jpg"
+          layout="fill"
+          alt="background-img"
+          className="absolute top-0 left-0 w-screen h-screen -z-10"
+        />
+        <div className="h-[600px] w-[550px] pt-[4rem] flex flex-col justify-center shadow-md bg-gray-700 rounded-md">
+          <Image
+            src="/../public/discord-logo.png"
+            alt="logo"
+            height={"200px"}
+            width={"300px"}
+            className=" object-contain flex"
+          />
+          <div className="w-full h-full z-20  pb-[4rem]">
+            <h1 className="text-white font-bold mt-4 text-center text-4xl">
+              Welcome Back!
+            </h1>
+            <p className="text-gray-300 font-semibold text-md my-4 text-center">
+              We're so exicted to see you again!
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <div className="flex flex-col w-[100%] ">
+              <div className="flex flex-col">
+                <label htmlFor="email" className="signin_label">
+                  Email
+                </label>
+                <input type="email" className="signin_input" />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="password" className="signin_label">
+                  Password
+                </label>
+                <input type="password" className="signin_input !mb-0" />
+                <span className="text-blue-400 w-max text-sm mb-[1rem] ml-[2rem] hover:underline cursor-pointer">
+                  Forget Password?
+                </span>
+              </div>
+              <button className="bg-blue-600 text-white font-semibold text-center text-lg p-2 w-[80%] mx-auto cursor-pointer hover:bg-blue-500 rounded-md ">
+                Login
+              </button>
+              <p className="w-max text-gray-400 mt-2 text-sm mb-[1rem] ml-[2rem] ">
+                Need an Account{" "}
+                <span className="text-blue-400 hover:underline cursor-pointer ">
+                  Register?
+                </span>
+              </p>
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
