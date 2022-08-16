98ef40fd6186c5223bc59f6c967b8bea7db22656
Aditya Painuli <adityapainuli2004@gmail.com>
2022-08-16 14:35:03 +0530

    homepage intro added

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,56 +9,22 @@ const Home: NextPage = () => {
         <title>Discord Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <Image
-          src="/background.jpg"
-          layout="fill"
-          alt="background-img"
-          className="absolute top-0 left-0 w-screen h-screen -z-10"
-        />
-        <div className="h-[600px] w-[550px] pt-[4rem] flex flex-col justify-center shadow-md bg-gray-700 rounded-md">
-          <Image
-            src="/discord-logo.png"
-            alt="logo"
-            height={"200px"}
-            width={"300px"}
-            className=" object-contain flex"
-          />
-          <div className="w-full h-full z-20  pb-[4rem]">
-            <h1 className="text-white font-bold mt-4 text-center text-4xl">
-              Welcome Back!
-            </h1>
-            <p className="text-gray-300 font-semibold text-md my-4 text-center">
-              We're so exicted to see you again!
-            </p>
-            <div className="flex flex-col w-[100%] ">
-              <div className="flex flex-col">
-                <label htmlFor="email" className="signin_label">
-                  Email
-                </label>
-                <input type="email" className="signin_input" />
-              </div>
-              <div className="flex flex-col">
-                <label htmlFor="password" className="signin_label">
-                  Password
-                </label>
-                <input type="password" className="signin_input !mb-0" />
-                <span className="text-blue-400 w-max text-sm mb-[1rem] ml-[2rem] hover:underline cursor-pointer">
-                  Forget Password?
-                </span>
-              </div>
-              <button className="bg-blue-600 text-white font-semibold text-center text-lg p-2 w-[80%] mx-auto cursor-pointer hover:bg-blue-500 rounded-md ">
-                Login
-              </button>
-              <p className="w-max text-gray-400 mt-2 text-sm mb-[1rem] ml-[2rem] ">
-                Need an Account{" "}
-                <span className="text-blue-400 hover:underline cursor-pointer ">
-                  Register?
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
+      <div className="w-[90%]  flex flex-col justify-center items-center">
+        <h1 className="text-gray-700 font-bold text-4xl my-4">
+          Working on the Application which will look like discord clone.
+        </h1>
+        <p className="text-gray-500 text-lg w-[80%] font-semibold ">
+          This is an open source project which I (aditya Painuli) started . The
+          main aim of this project is to learn and build in public but also at
+          the same time . I am trying to make this application same as discord
+          to much of the extend{" "}
+          <span className="text-green-500 font-bold capitalize">
+            Feel free to contribute in the project if you want to.
+          </span>
+        </p>
+        <span className="text-blue-500 font-bold text-xl py-[2rem]">
+          We will see to which extend we can copy discord developers work. 😆😆
+        </span>
       </div>
     </div>
   );
---
+++ pages/signup.tsx
@@ -0,0 +1,76 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Discord Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <Image
+          src="/background.jpg"
+          layout="fill"
+          alt="background-img"
+          className="absolute top-0 left-0 w-screen h-screen -z-10"
+        />
+        <div className="h-[700px] w-[550px] pt-[4rem] flex flex-col justify-center shadow-md bg-gray-700 rounded-md">
+          <Image
+            src="/discord-logo.png"
+            alt="logo"
+            height={"200px"}
+            width={"300px"}
+            className=" object-contain flex"
+          />
+          <div className="w-full h-full z-20  pb-[4rem]">
+            <h1 className="text-white font-bold mt-4 text-center text-4xl">
+              Welcome Back!
+            </h1>
+            <p className="text-gray-300 font-semibold text-md my-4 text-center">
+              We're so exicted to see you again!
+            </p>
+            <div className="flex flex-col w-[100%] ">
+              <div className="flex flex-col">
+                <label htmlFor="username" className="signin_label">
+                  Username
+                </label>
+                <input type="text" className="signin_input" />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="email" className="signin_label">
+                  Email
+                </label>
+                <input type="email" className="signin_input" />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="password" className="signin_label">
+                  Password
+                </label>
+                <input type="password" className="signin_input !mb-0" />
+                <span className="text-blue-400 w-max text-sm mb-[1rem] ml-[2rem] hover:underline cursor-pointer">
+                  Forget Password?
+                </span>
+              </div>
+              <button className="bg-blue-600 text-white font-semibold text-center text-lg p-2 w-[80%] mx-auto cursor-pointer hover:bg-blue-500 rounded-md ">
+                Login
+              </button>
+              <p className="w-max text-gray-400 mt-2 text-sm mb-[1rem] ml-[2rem] ">
+                Already have an Account{" "}
+                <Link href={"/signin"}>
+                  <span className="text-blue-400 hover:underline cursor-pointer ">
+                    Sign in?
+                  </span>
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
